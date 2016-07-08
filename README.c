06c0a04853d45942d0a38720a95179c13beb6f91
ddeepikaa <ddeepikaa@users.noreply.github.com>
2016-07-08 15:57:05 +0530

    finish

---
+++ README.c
@@ -0,0 +1,18 @@
+# palind
+#include<stdio.h>
+int main()
+{
+  int num[3]={1,2,1},ctr;
+  size=3;
+  for(ctr=0,ctr1=2;ctr<size/2;ctr++,ctr1--)
+  {  
+    if(arr[ctr]==arr[ctr1])
+      continue;
+      }
+      if(ctr==1)
+        printf("palindrome");
+        else 
+         printf("not a palindrome");
+  
+
+}
